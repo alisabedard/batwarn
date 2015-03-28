8fcfa760b317c7a6bc2d8ee9120c6a32e950adb3
Jeffrey E. Bedard <jefbed@gmail.com>
2015-03-28 01:18:01 -0400

    Added base command line argument support.  Refactored project into modules.

---
+++ signal.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   signal.h
+ * Author: jefbed
+ *
+ * Created on March 27, 2015, 11:57 PM
+ */
+
+#ifndef SIGNAL_H
+#define	SIGNAL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void
+setup_signal_handler();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SIGNAL_H */
+
