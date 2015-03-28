290970c9cdd547d5ae9b1dd8c31d7558bd6869f0
Jeffrey E. Bedard <jefbed@gmail.com>
2015-03-28 01:18:58 -0400

    New file

---
+++ signal.c
@@ -0,0 +1,23 @@
+#include "batwarn.h"
+#include <signal.h>
+#include <stdlib.h>
+
+static void 
+signal_handler()
+{
+	batwarn_set_gamma(GAMMA_NORMAL);
+#ifdef DEBUG
+	puts("SIGNAL");
+#endif /* DEBUG */
+	exit(0);
+}
+
+void
+setup_signal_handler()
+{
+	struct sigaction sa;
+
+	sa.sa_handler=signal_handler;
+	sigaction(SIGINT, &sa, NULL);
+}
+
