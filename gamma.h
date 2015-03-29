4c1dfa9fd84bd7228aef345ade5b9b25eef81fd4
Jeffrey E. Bedard <jefbed@gmail.com>
2015-03-29 03:11:13 -0400

    New file

---
+++ gamma.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   gamma.h
+ * Author: jefbed
+ *
+ * Created on March 29, 2015, 2:51 AM
+ */
+
+#ifndef GAMMA_H
+#define	GAMMA_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+void
+batwarn_set_gamma(const float value);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GAMMA_H */
+
