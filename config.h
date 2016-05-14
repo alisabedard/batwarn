7cc9b51d1d2e091baf4f72a02e46ac1471936e7f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-05-14 00:00:27 -0400

    Convert defines to enums.  Named percentage category enum.  Documented values.

--- config.h
+++ config.h
@@ -1,26 +1,28 @@
 // batwarn - (C) 2015-2016 Jeffrey E. Bedard
 
-#ifndef CONFIG_H
-#define	CONFIG_H
+#ifndef BW_CONFIG_H
+#define	BW_CONFIG_H
 
 #define GAMMA_NORMAL    1.0
 #define GAMMA_WARNING   5.0
 
-enum {
-	CRIT_PERCENT=5
-	,LOW_PERCENT=10
-	,FULL_PERCENT=90
+enum PercentCat {
+	CRIT_PERCENT=5,
+	LOW_PERCENT=10,
+	FULL_PERCENT=90
 };
 
+// Delay for checking system files:
 #ifndef DEBUG
-#define WAIT 60
+enum { WAIT = 60 };
 #else//DEBUG
-#define WAIT 1
+enum { WAIT = 1 };
 #endif//!DEBUG
 
+// System files to check:
 #define BATSYSFILE "/sys/class/power_supply/BAT0/capacity"
 #define ACSYSFILE "/sys/class/power_supply/AC/online"
 
 #define SUSPEND_CMD "systemctl suspend"
 
-#endif//CONFIG_H
+#endif//!BW_CONFIG_H
