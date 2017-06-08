46109a7e4804ce00bf72a995db684396122c1c9b
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-08 02:05:00 -0400

    Updated.

--- depend.mk
+++ depend.mk
@@ -1,6 +1,7 @@
 batwarn.o: batwarn.c batwarn.h config.h gamma.h log.h util.h
 gam.o: gam.c gamma.h
 gamma.o: gamma.c gamma.h
-main.o: main.c batwarn.h config.h gamma.h
+main.o: main.c batwarn.h config.h gamma.h version.h
 sgam.o: sgam.c gamma.h
 util.o: util.c util.h
+version.o: version.c version.h config.h
