b2170bcd3f021bcb53ea22923ff5e37e04f58cc8
Jeffrey E. Bedard <jefbed@gmail.com>
2019-11-18 23:04:56 -0500

    Reindented, used sudo, version string, settings enum.

--- config.h
+++ config.h
@@ -1,21 +1,26 @@
 // batwarn - (C) 2015-2017 Jeffrey E. Bedard
 #ifndef BATWARN_CONFIG_H
 #define	BATWARN_CONFIG_H
-enum BatwarnVersion {
-	BATWARN_VERSION_MAJOR = 1,
-	BATWARN_VERSION_MINOR = 1,
-	BATWARN_VERSION_PATCH = 1
+#define BATWARN_VERSION "20191117"
+enum BatwarnSettings {
+    ENABLE_HIBERNATE_BY_DEFAULT=1,
+    ENABLE_SUSPEND_BY_DEFAULT=0
 };
 #define BATWARN_GAMMA_NORMAL    1.0
 #define BATWARN_GAMMA_WARNING   5.0
 enum BatwarnLevel {
-	BATWARN_PERCENT_CRITICAL=15,
-	BATWARN_PERCENT_LOW=20,
-	BATWARN_PERCENT_FULL=90
+    BATWARN_PERCENT_CRITICAL=15,
+    BATWARN_PERCENT_LOW=20,
+    BATWARN_PERCENT_FULL=90
 };
 // System files to check:
 #define BATWARN_SYS_BATTERY_FILE "/sys/class/power_supply/BAT0/capacity"
 #define BATWARN_SYS_AC_FILE "/sys/class/power_supply/AC/online"
-#define BATWARN_SUSPEND_COMMAND "systemctl suspend"
-#define BATWARN_HIBERNATE_COMMAND "systemctl hibernate"
+#ifndef USE_SYSTEMD
+#define BATWARN_SUSPEND_COMMAND "sudo pm-suspend"
+#define BATWARN_HIBERNATE_COMMAND "sudo pm-hibernate"
+#else//USE_SYSTEMD
+#define BATWARN_SUSPEND_COMMAND "sudo systemctl suspend"
+#define BATWARN_HIBERNATE_COMMAND "sudo systemctl hibernate"
+#endif//!USE_SYSTEMD
 #endif//!BATWARN_CONFIG_H
