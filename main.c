9d73c40ec77e96dab97dd1b007b87886684bac33
Jeffrey E. Bedard <jefbed@gmail.com>
2019-11-18 23:05:43 -0500

    Restored usage, fixed missing c: flag.

--- main.c
+++ main.c
@@ -6,75 +6,67 @@
 #include <unistd.h>
 #include "config.h"
 #include "gamma.h"
-#include "version.h"
 static void exit_cb(void)
 {
-	batwarn_set_gamma(BATWARN_GAMMA_NORMAL);
+    batwarn_set_gamma(BATWARN_GAMMA_NORMAL);
 }
-static void print_version(void)
-{
-	short s;
-	const char * v = batwarn_get_version(&s);
-	write(1, v, s);
+static void print_version(void) {
+    write(1, BATWARN_VERSION, sizeof(BATWARN_VERSION));
+    write(1,"\n",2);
 }
 _Noreturn void usage(const int ec)
 {
-	int fd = open(BATWARN_USAGE, O_RDONLY);
-	/* The following is useful for uninstalled binaries, such as on
-	 * systems on which you do not have root access, and for viewing
-	 * usage prior to installation.  */
-	if (fd < 0) // fail to the current directory:
-		fd = open("usage.txt", O_RDONLY);
-	if (fd >= 0) {
-		/* Note:  Adjust this size upwards if usage.txt
-		   no longer fits in 512 bytes.  */
-		enum { SZ = 512 };
-		char buf[SZ];
-		// Write how much we read.
-		write(1, buf, read(fd, buf, SZ));
-		// No need to close since exit will do it for us...
-	}
-	print_version();
-	exit(ec);
+    char const msg[]=
+        "batwarn -dhHp:s\n"
+        "-c PERCENT	Set the critical battery level.\n"
+        "-d		Do not fork a daemon; run in the foreground.\n"
+        "-h		Show this usage information.\n"
+        "-H		Enable hibernation at critical battery level.\n"
+        "-p PERCENT	Set the warning percent for gamma change.\n"
+        "-s		Enable suspend at critical battery level.\n"
+        "Copyright 2017-2019, Jeffrey E. Bedard <jefbed@gmail.com>\n";
+        write(1,msg,sizeof(msg));
+    print_version();
+    exit(ec);
 }
 static uint8_t parse_argv(int argc, char ** argv, uint8_t flags)
 {
-	int8_t opt;
-	const char optstr[] = "dhHp:s";
-	while((opt = getopt(argc, argv, optstr)) != -1)
-		switch (opt) {
-		case 'c': // set critical percentage
-			batwarn_set_critical(atoi(optarg));
-			break;
-		case 'd': // debug
-			flags |= BATWARN_DEBUG;
-			break;
-		case 'H': // enable hibernate at critical
-			flags |= BATWARN_HIBERNATE;
-			break;
-		case 'p': // warning percentage
-			batwarn_set_percent(atoi(optarg));
-			break;
-		case 's': // enable suspend
-			flags |= BATWARN_SUSPEND;
-			break;
-		case 'h': // help
-			usage(0);
-			//usage(*argv, optstr, sizeof(optstr), 0);
-		default: // usage
-			usage(1);
-			///usage(*argv, optstr, sizeof(optstr), 1);
-		}
-	return flags;
+    int8_t opt;
+    const char optstr[] = "c:dhHp:s";
+    while((opt = getopt(argc, argv, optstr)) != -1)
+        switch (opt) {
+            case 'c': // set critical percentage
+                batwarn_set_critical(atoi(optarg));
+                break;
+            case 'd': // debug
+                flags |= BATWARN_DEBUG;
+                break;
+            case 'H': // enable hibernate at critical
+                flags |= BATWARN_HIBERNATE;
+                break;
+            case 'p': // warning percentage
+                batwarn_set_percent(atoi(optarg));
+                break;
+            case 's': // enable suspend
+                flags |= BATWARN_SUSPEND;
+                break;
+            case 'h': // help
+                usage(0);
+                //usage(*argv, optstr, sizeof(optstr), 0);
+            default: // usage
+                usage(1);
+                ///usage(*argv, optstr, sizeof(optstr), 1);
+        }
+    return flags;
 }
 int main(int argc, char **argv)
 {
-	const uint8_t flags = parse_argv(argc, argv, 0);
-	if(!(flags & BATWARN_DEBUG) && (fork() != 0))
-		return 0;
-	signal(SIGINT, exit);
-	signal(SIGTERM, exit);
-	atexit(exit_cb);
-	batwarn_start_checking(flags);
-	return 0;
+    const uint8_t flags = parse_argv(argc, argv, 0);
+    if(!(flags & BATWARN_DEBUG) && (fork() != 0))
+        return 0;
+    signal(SIGINT, exit);
+    signal(SIGTERM, exit);
+    atexit(exit_cb);
+    batwarn_start_checking(flags);
+    return 0;
 }
