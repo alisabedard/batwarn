cc499ade505a79652062c9a0af906f6cecae873d
Jeffrey E. Bedard <jefbed@gmail.com>
2020-08-04 08:33:24 -0400

    Reindented, used stdio, moved fd_t.

--- util.c
+++ util.c
@@ -1,61 +1,50 @@
 // Copyright 2017-2018, Jeffrey E. Bedard
 #include "util.h"
 #include <fcntl.h> // for open()
+#include <stdio.h> // for puts()
 #include <stdlib.h> // for abort(), exit()
 #include <sys/wait.h> // for wait()
 #include <unistd.h> // for write(), fork(), execl()
-typedef char fd_t;
-static void batwarn_print(const char * restrict msg)
-{
-	unsigned short l = 0;
-	while (msg[++l]);
-	write(STDERR_FILENO, msg, l);
-}
 static void check(int fail_condition, const char * restrict message)
 {
-	if (fail_condition) {
-		batwarn_print(message);
-		exit(1);
-	}
+  if (fail_condition) {
+    puts(message);
+    exit(1);
+  }
 }
 static void sig_child_cb(int sig)
 {
-	if (sig != SIGCHLD)
-		abort(); // attached to the wrong signal
-	int s;
-	// Loop through potentially multiple children
-	while (wait(&s) > 0) {
-		if (WIFEXITED(s)) {
-			if (WEXITSTATUS(s) != 0)
-				batwarn_print("Command exited abnormally\n");
-			else
-				batwarn_print("Command exited normally\n");
-		} else if (WIFSIGNALED(s))
-			batwarn_print("Terminated by a signal\n");
-	}
+  if (sig != SIGCHLD)
+    abort(); // attached to the wrong signal
+  int s;
+  // Loop through potentially multiple children
+  while (wait(&s) > 0) {
+    if (WIFEXITED(s)) {
+      if (WEXITSTATUS(s) != 0)
+        puts("Command exited abnormally");
+      else
+        puts("Command exited normally");
+    } else if (WIFSIGNALED(s))
+      puts("Terminated by a signal");
+  }
 }
 void batwarn_execute(const char * restrict cmd)
 {
-	pid_t fval = fork();
-	check(fval < 0, "fork failed");
-	if(fval) // in parent process
-		check(signal(SIGCHLD, sig_child_cb) == SIG_ERR,
-			"signal()"); // attach signal handler
-	else // in child process
-		check(execl("/bin/sh", "sh", "-c", cmd, NULL) < 0, "execl()");
+  pid_t fval = fork();
+  check(fval < 0, "fork failed");
+  if(fval) // in parent process
+    check(signal(SIGCHLD, sig_child_cb) == SIG_ERR,
+      "signal()"); // attach signal handler
+  else // in child process
+    check(execl("/bin/sh", "sh", "-c", cmd, NULL) < 0, "execl()");
 }
-static fd_t get_fd(const char * fn)
+int batwarn_get_value(const char * fn)
 {
-	const fd_t fd = open(fn, O_RDONLY);
-	check(fd < 0, fn);
-	return fd;
-}
-int bw_get_value(const char * fn)
-{
-	const fd_t fd = get_fd(fn);
-	enum {READ_SZ = 4};
-	char buf[READ_SZ];
-	check(read(fd, buf, READ_SZ) < 0, fn);
-	check(close(fd) < 0, "close()");
-	return atoi(buf);
+  enum {READ_SZ = 4};
+  char buf[READ_SZ];
+  fd_t const fd = open(fn, O_RDONLY);
+  check(fd < 0, fn);
+  read(fd, buf, READ_SZ);
+  close(fd);
+  return atoi(buf);
 }
--- util.h
+++ util.h
@@ -1,6 +1,7 @@
 // Copyright 2017, Jeffrey E. Bedard
 #ifndef BATWARN_UTIL_H
 #define BATWARN_UTIL_H
+typedef int fd_t;
 void batwarn_execute(const char * restrict cmd);
-int bw_get_value(const char * fn);
+int batwarn_get_value(const char * fn);
 #endif//!BATWARN_UTIL_H
