ca4fbb631b205f76c7865d43c5a151c61d727efc
Jeffrey E. Bedard <jefbed@gmail.com>
2020-08-07 21:59:59 -0400

    Regenerated dependencies.

--- depend.mk
+++ depend.mk
@@ -1,6 +1,4 @@
-batwarn.o: batwarn.c batwarn.h config.h gamma.h log.h util.h
+batwarn.o: batwarn.c BatwarnFlags.h config.h gamma.h
 gam.o: gam.c gamma.h
 gamma.o: gamma.c gamma.h
-main.o: main.c batwarn.h config.h gamma.h util.h
 sgam.o: sgam.c gamma.h
-util.o: util.c util.h
